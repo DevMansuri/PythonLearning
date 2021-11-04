87a1ea83b87e099d84561f00afecb51510d430a8
“Nadim” <nadimmansu@gmail.com>
2021-11-04 10:19:36 +0530

    Add New File : How to print star Pattern in python

---
+++ Pattern5.py
@@ -0,0 +1,14 @@
+'''
+Author : Nadim Mansuri.
+Date : 02/11/2021.
+Purpose : Purpose of this function , Print Pattern:
+					  *****
+					  ****
+					  ***
+					  **
+					  *    
+'''
+def Pattern():
+	for i in range(1,6):
+		print("*"*(6-i) + " "*i)
+Pattern()		
