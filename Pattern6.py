9c9deb035d7396763c74a93b07f5c6b2a598a6e7
“Nadim” <nadimmansu@gmail.com>
2021-11-07 09:58:54 +0530

    Add New File :Star Pattern Example

---
+++ Pattern6.py
@@ -0,0 +1,14 @@
+'''
+Author : Nadim Mansuri.
+Date : 02/11/2021.
+Purpose : Purpose of this function , Print Pattern:
+					  *****
+					   ****
+					    ***
+					     **
+					      *    
+'''
+def Pattern():
+	for i in range(1,6):
+		print(" " * i + "*" * (6-i))
+Pattern()		
